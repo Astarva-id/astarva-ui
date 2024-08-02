5b46b44f51092a5731a342f9aac2952c25ccae06
GannaHap <gannahap02@gmail.com>
2024-08-02 17:41:03 +0700

    feat: [ASTV-13] setup for deploy

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// Export Component
+export { default as Box } from "@components/Box";
+export { default as Colors } from "@components/Colors";
+export { default as Flex } from "@components/Flex";
+
+// Export Component Interfaces
+export * from "@components/Box/Box.types";
+export * from "@components/Colors/Colors.types";
+export * from "@components/Flex/Flex.types";
+
+// Export Utils
+export * from "@utils/globals";
