6275c76f1d28f3613c28d79e53d21dc4216bb5d9
GannaHup <gannahap02@gmail.com>
2024-11-24 13:51:48 +0700

    fix: [GAS-7] height component switch

--- src/components/Switch/Switch.styled.ts
+++ src/components/Switch/Switch.styled.ts
@@ -1,4 +1,3 @@
-import Box from "@components/Box";
 import colors from "@components/Colors";
 import Flex from "@components/Flex";
 import { slideLeftToRight, slideRightToLeft } from "@styles/animation.styled";
@@ -6,19 +5,17 @@ import styled, { css } from "styled-components";
 
 import { switchCircleVariant, switchVariant } from "./Switch.variant";
 
-export const Circle = styled(Box)``;
-
 export const SwitchComponent = styled(Flex)`
-  ${switchVariant}
   position: relative;
+  width: 100%;
   border-radius: 1.25rem;
   padding: 0.25rem;
-  width: 100%;
   cursor: pointer;
-  transition: background-color 0.3s ease-in;
   background-color: ${(props) =>
     props.active ? colors.blue400 : colors.black100};
   opacity: ${(props) => (props.disabled ? ".5" : "1")};
+  transition: background-color 0.3s ease-in;
+  ${switchVariant}
 
   .circle {
     ${switchCircleVariant}
